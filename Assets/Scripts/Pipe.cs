4165b3fdb372cad893c166637eb538d79c9ecd2c
alfianAH <alfian.hamdani04@gmail.com>
2020-08-01 16:55:53 +0800

    Make pipe and its spawner

---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    // Global variables
+    [SerializeField] private Bird bird;
+    [SerializeField] private float speed = 1;
+
+    private void Update()
+    {
+        // Check if bird isn't dead yet
+        if (!bird.IsDead)
+        {
+            // Make pipe move to left
+            transform.Translate(Vector3.left * (speed * Time.deltaTime), Space.World);
+        }
+    }
+
+    // Make bird is dead when collide with this object and make it falling down
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Bird bird = other.gameObject.GetComponent<Bird>();
+
+        if (bird)
+        {
+            // Get collider component
+            Collider2D collider = GetComponent<Collider2D>();
+
+            if (collider)
+                collider.enabled = false; // Deactivate collider
+            bird.Dead();
+        }
+    }
+}
---
+++ Assets/Scripts/PipeSpawner.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    // Global variables 
+    [SerializeField] private Bird bird;
+    [SerializeField] private Pipe pipeUp, pipeDown;
+    [SerializeField] private float spawnInterval = 1f;
+    [SerializeField] private float holeSize = 1f;
+    [SerializeField] private float maxMinOffset = 1f;
+    
+    // Coroutine variable
+    private Coroutine crSpawn;
+
+    private void Start()
+    {
+        StartSpawn();
+    }
+
+    private void StartSpawn()
+    {
+        // Start Spawn Coroutine
+        if (crSpawn == null)
+            crSpawn = StartCoroutine(Spawn());
+        
+    }
+    
+    private IEnumerator Spawn()
+    {
+        while (true)
+        {
+            // If bird is dead, Stop Spawn
+            if(bird.IsDead)
+                StopSpawn();
+            
+            SpawnPipe(); // Spawn new pipe
+            
+            // Wait for seconds to spawn new pipe
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    private void StopSpawn()
+    {
+        // Stop coroutine Spawn 
+        if (crSpawn != null)
+            StopCoroutine(crSpawn);
+    }
+
+    private void SpawnPipe()
+    {
+        // Duplicate pipeUp and place it in the same position but rotated 180 degrees
+        Pipe newPipeUp = Instantiate(pipeUp, transform.position, Quaternion.Euler(0, 0, 180));
+        
+        // Activate newPipeUp
+        newPipeUp.gameObject.SetActive(true);
+        
+        // Duplicate pipeDown and place it in the same position
+        Pipe newPipeDown = Instantiate(pipeDown, transform.position, Quaternion.identity);
+        
+        // Activate newPipeDown
+        newPipeDown.gameObject.SetActive(true);
+
+        // Make hole in the middle of pipe
+        newPipeUp.transform.position += Vector3.up * (holeSize / 2); 
+        newPipeDown.transform.position += Vector3.down * (holeSize / 2);
+        
+        // Place pipe's position so that its position adjusts to the Sin function
+        float y = maxMinOffset * Mathf.Sin(Time.time);
+        newPipeUp.transform.position += Vector3.up * y;
+        newPipeDown.transform.position += Vector3.up * y;
+    }
+}
