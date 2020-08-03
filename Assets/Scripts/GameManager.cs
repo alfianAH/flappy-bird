ebe3220c8bbd5d64df179e11f45e05e34db85c8c
alfianAH <alfian.hamdani04@gmail.com>
2020-08-03 22:53:12 +0800

    Add game manager for exit the game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Image exitMenu;
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 0f;
+            exitMenu.gameObject.SetActive(true);
+        }
+    }
+
+    public void ApplicationQuit()
+    {
+        Application.Quit();
+    }
+
+    public void CancelQuit()
+    {
+        Time.timeScale = 1f;
+        exitMenu.gameObject.SetActive(false);
+    }
+}
