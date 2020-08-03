9c15073f459044ba2cb7d9c874c4e115992b35b6
alfianAH <alfian.hamdani04@gmail.com>
2020-08-03 23:38:06 +0800

    Change background day and night periodically

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private Bird bird;
     [SerializeField] private Image exitMenu;
+    [SerializeField] private Sprite[] backgroundSprites;
+    [SerializeField] private SpriteRenderer[] backgrounds;
+    private float longDay = 30f;
+    private int index = 0;
+    private bool hasChange;
     
     private void Update()
     {
@@ -14,6 +18,50 @@ public class GameManager : MonoBehaviour
         {
             Time.timeScale = 0f;
             exitMenu.gameObject.SetActive(true);
+        }
+
+        if (!bird.IsDead && !hasChange)
+        {
+            StartCoroutine(ChangeBackground());
+        }
+    }
+
+    private IEnumerator ChangeBackground()
+    {
+        switch (index)
+        {
+            case 0: // Morning
+                Debug.Log("Case 0");
+                
+                hasChange = true;
+                yield return new WaitForSeconds(longDay);
+                index = 1; // Set to night
+                
+                Debug.Log("done waiting_Case 0");
+                // Change sprites
+                for (int i = backgrounds.Length-1; i >= 0; i--)
+                {
+                    backgrounds[i].sprite = backgroundSprites[index];
+                    yield return new WaitForSeconds(1.5f);
+                }
+                hasChange = false;
+                break;
+            case 1: // Night
+                Debug.Log("Case 1");
+                
+                hasChange = true;
+                yield return new WaitForSeconds(longDay);
+                index = 0; // Set to night
+                
+                Debug.Log("done waiting_Case 1");
+                // Change sprites
+                for (int i = backgrounds.Length-1; i >= 0; i--)
+                {
+                    backgrounds[i].sprite = backgroundSprites[index];
+                    yield return new WaitForSeconds(1.5f);
+                }
+                hasChange = false;
+                break;
         }
     }
 
