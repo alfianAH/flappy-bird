b55e3b845797ec5007f6e721e16d03112e65496d
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-08-07 20:58:57 +0800

    Delete prefs when first build

---
+++ Assets/Scripts/ResetPlayerPrefs.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ResetPlayerPrefs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("firstTime")) return;
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.SetInt("firstTime", 1);
+    }
+}
